587c0c47950a1d3fa75d265727999269ae096d89
iamsobanjaved <iamsobanjaved@gmail.com>
2020-02-18 20:30:41 +0500

    fixed app_config of oscar app

--- ecommerce/extensions/config.py
+++ ecommerce/extensions/config.py
@@ -1,17 +1,15 @@
 from __future__ import absolute_import
 
-from oscar import app
-from oscar.core.application import Application
+from oscar import config
+from oscar.core.application import OscarConfig
 
 
-class EdxShop(app.Shop):
+class EdxShop(config.Shop):
+    name = "ecommerce"
     # URLs are only visible to users with staff permissions
     default_permissions = 'is_staff'
 
     # Override core app instances with blank application instances to exclude their URLs.
-    promotions_app = Application()
-    catalogue_app = Application()
-    search_app = Application()
-
-
-application = EdxShop()
+    promotions_app = OscarConfig()
+    catalogue_app = OscarConfig()
+    search_app = OscarConfig()
